7823a326d00778affa2e44ef0b1023bcb6d664f9
Jakub Borek <01158592@pw.edu.pl>
2023-01-06 20:11:01 +0100

    Create basic Pdf creator class

---
+++ Bank.API/Controllers/PdfService/PdfCreator.cs
@@ -0,0 +1,24 @@
+﻿using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+
+namespace Bank.API.Controllers.PdfService;
+
+public class PdfCreator
+{
+    public PdfDocument CreateDocument(int offerId)
+    {
+        var document = new PdfDocument();
+        var page = document.AddPage();
+        
+        XGraphics gfx = XGraphics.FromPdfPage(page);
+        XFont font = new XFont("Verdana", 20, XFontStyle.Bold);
+        
+        gfx.DrawString($"Offer {offerId} document", font, XBrushes.Black,
+            new XRect(0, 0, page.Width, page.Height),
+            XStringFormat.Center);
+        
+        string filename = $"{offerId}.pdf";
+
+        return document;
+    }
+}
